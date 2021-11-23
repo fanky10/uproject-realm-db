c92b8cf1d573c8ed642dc5b46716b3cc0f934534
Facundo Scoccia <facundo.scoccia@globant.com>
2021-11-23 15:19:53 -0500

    simple impl

--- app/src/main/java/io/github/fanky10/android/uproject/realmdb/MainActivity.kt
+++ app/src/main/java/io/github/fanky10/android/uproject/realmdb/MainActivity.kt
@@ -1,9 +1,10 @@
 package io.github.fanky10.android.uproject.realmdb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import io.github.fanky10.android.uproject.realmdb.model.RProduct
 import io.realm.Realm
 
@@ -11,19 +12,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        listen()
         findViewById<Button>(R.id.btnAdd).setOnClickListener {
             add()
+        }
+        findViewById<Button>(R.id.btnClear).setOnClickListener {
+            clear()
         }
     }
 
     private fun add() {
-        val newProduct = RProduct(
-            "a","product"
-        )
-        val realmInstanceConfig = (this.application as RealmDBApplication).realmInstanceConfig
-        Realm.getInstance(realmInstanceConfig).executeTransaction {
-            it.copyToRealm(newProduct)
-            Log.d("rfanky", "added!")
-        }
+        Thread ({
+            Realm.getDefaultInstance().executeTransaction {
+                val newProduct = RProduct(
+                    "a", "product"
+                )
+                it.copyToRealm(newProduct)
+                Log.d("rfanky", "added!")
+            }
+        }, "Other T").start()
+    }
+
+    private fun listen() {
+        val disposable = Realm.getDefaultInstance().where(RProduct::class.java)
+            .findAllAsync()
+            .asFlowable()
+            .filter { result -> result.isValid && result.isLoaded }
+            .map { results ->
+                results.size
+            }
+            .toObservable()
+            .subscribe { size ->
+                val message = "found: $size"
+                Log.d("rfanky", message)
+                findViewById<TextView>(R.id.txtStatus).text = message
+            }
+    }
+
+    private fun clear() {
+        Thread({
+            Realm.getDefaultInstance().executeTransaction {
+                it.delete(RProduct::class.java)
+            }
+        }, "Name01").start()
     }
 }
--- app/src/main/java/io/github/fanky10/android/uproject/realmdb/RealmDBApplication.kt
+++ app/src/main/java/io/github/fanky10/android/uproject/realmdb/RealmDBApplication.kt
@@ -7,31 +7,9 @@ import io.realm.Realm
 import io.realm.RealmConfiguration
 
 class RealmDBApplication : Application() {
-    lateinit var realmInstanceConfig: RealmConfiguration
 
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
-        realmInstanceConfig = RealmConfiguration.Builder()
-            .name("realm_db")
-            .schemaVersion(1)
-            .allowWritesOnUiThread(true) // -- just for the initial version
-//            .modules(new ExcaliburSchema()) -- unused
-            .compactOnLaunch()
-//            .migration(new SchemaMigration(realmMigrationPreferenceUseCase)) -- no migration needed
-            .build()
-        Realm.getInstance(realmInstanceConfig).use {
-            it.where(RProduct::class.java)
-                .findAllAsync()
-                .asFlowable()
-                .filter{ result -> result.isValid && result.isLoaded }
-                .map { results ->
-                    results.size
-                }
-                .toObservable()
-                .subscribe { size ->
-                    Log.d("rfanky", "found: $size")
-                }
-        }
     }
 }
